3ee044c3fe7e1e4c152aa0fe1e9d03d9e3a93175
Arturo Aguilar <arturo_365ag@hotmail.com>
2019-06-17 19:46:38 -0500

    Otro cambio

--- AppCliente/AppCliente/ProductoDescripcionPage.xaml.cs
+++ AppCliente/AppCliente/ProductoDescripcionPage.xaml.cs
@@ -15,12 +15,12 @@ namespace AppCliente
     public partial class ProductoDescripcionPage : ContentPage
     {
         VMProducto objProducto;
-        double cantidad=1;
+        double cantidad = 1;
         string costo;
         string CostoPorSucursal = "";
         Guid idSeccion;
-        bool tap=true;
-        bool tipo=false;
+        bool tap = true;
+        bool tipo = false;
         string UIDEmpresa;
 
         public static List<VMProducto> ListaPreciosProcto = new List<VMProducto>();
@@ -35,7 +35,7 @@ namespace AppCliente
         public ProductoDescripcionPage(VMProducto objProducto, List<VMProducto> lista)
         {
             InitializeComponent();
-            UIDEmpresa=objProducto.UIDEMPRESA.ToString();
+            UIDEmpresa = objProducto.UIDEMPRESA.ToString();
             this.objProducto = objProducto;
             string url = objProducto.STRRUTA;
             ImagenProducto.Source = url;
@@ -63,20 +63,19 @@ namespace AppCliente
             ImagenProducto.Source = url;
             txtNombreProducto.Text = objProducto.STRNOMBRE;
             txtDescripcionProducto.Text = objProducto.STRDESCRIPCION;
-            
+
             txtEmpresaCosto.Text = "Precio: $" + objProducto.StrCosto;
             CostoPorSucursal = objProducto.StrCosto;
             //btnCambioDeSucursal.IsVisible = false;
             //lbIdEmpresa.Text = UiEmpresa.ToString();
             txtIDSeccion.Text = objSeccion.UID.ToString();
-            idSeucursalSeleccionada.Text= UiEmpresa.ToString();
+            idSeucursalSeleccionada.Text = UiEmpresa.ToString();
             txtSucursall.IsVisible = false;
             txtSucursalSeleccionada.IsVisible = false;
             btnAgregarCarrito.Text = "Agregar carrito $" + objProducto.StrCosto;
             idSeccion = objSeccion.UID;
             tipo = false;
         }
-
 
         private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
         {
@@ -144,194 +143,6 @@ namespace AppCliente
             Navigation.PushAsync(new SeleccionarSucursalPrecioProducto(ListaPreciosProcto, btnAgregarCarrito, txtSucursalSeleccionada, txtEmpresaCosto, cantidad, txtIDSeccion, idSeucursalSeleccionada));
         }
 
-        //protected void AgregarAlcarrito(Guid UidProducto, Guid UidSucursal, Guid UidSeccion, string StrCantidad, string StrNotas = "")
-        //{
-        //    #region Guardar el link de imagen
-        //    string URLEmpresa;
-        //    if (UIDEmpresa == "00000000-0000-0000-0000-000000000000")
-        //    {
-        //        URLEmpresa = "http://www.godeliverix.net/vista/" + App.MVImagen.STRRUTA;
-        //    }
-        //    else
-        //    {
-        //        App.MVImagen.ObtenerImagenPerfilDeEmpresa(UIDEmpresa);
-        //        URLEmpresa = "http://www.godeliverix.net/vista/" + App.MVImagen.STRRUTA;
-        //    }
-        //    #endregion
-
-        //    var Producto = App.MVProducto.ListaDelCarrito.FindAll(Objeto => Objeto.UID == UidProducto && Objeto.UidNota == Guid.Empty && Objeto.UidSucursal == new Guid(idSeucursalSeleccionada.Text));
-        //    if (Producto.Count <= 1 && !string.IsNullOrEmpty(StrNotas) || (Producto.Count == 0 && string.IsNullOrEmpty(StrNotas)))
-        //    {
-        //        if (App.MVTarifario.ListaDeTarifarios.Count > 0)
-        //        {
-        //            Guid UidTarifario = new Guid();
-        //            decimal DmPrecio = 0.0m;
-        //            for (int i = 0; i < 1; i++)
-        //            {
-        //                UidTarifario = App.MVTarifario.ListaDeTarifarios[i].UidTarifario;
-        //                DmPrecio = App.MVTarifario.ListaDeTarifarios[i].DPrecio;
-        //            }
-        //            //UidSeccion
-        //            App.MVProducto.AgregaAlCarrito(UidProducto, new Guid(idSeucursalSeleccionada.Text), new Guid(txtIDSeccion.Text), StrCantidad, DmPrecio, UidTarifario, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //        } // los datos de la informacion del tarifario se muestran vacios en caso de existir varios registros para esta orden.
-        //        else
-        //        {
-        //            if (tipo)
-        //            {
-        //                App.MVProducto.AgregaAlCarrito(UidProducto, UidSeccion, UidSucursal, StrCantidad, 0.0m, Guid.Empty, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //            }
-        //            else
-        //            {
-        //                App.MVProducto.AgregaAlCarrito(UidProducto, UidSucursal, UidSeccion, StrCantidad, 0.0m, Guid.Empty, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //            }
-        //        }
-
-        //        decimal Subtotal = 0.0m;
-        //        decimal Envio = 0.0m;
-        //        int cantidad = 0;
-        //        foreach (var producto in App.MVProducto.ListaDelCarrito)
-        //        {
-        //            cantidad = cantidad + producto.Cantidad;
-        //            Subtotal = Subtotal + decimal.Parse(producto.StrCosto);
-        //        }
-        //        foreach (var obj in App.MVProducto.ListaDelInformacionSucursales)
-        //        {
-        //            Envio = Envio + obj.CostoEnvio;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        var productoRepetido = App.MVProducto.ListaDelCarrito.Find(Objeto => Objeto.UID == UidProducto && Objeto.UidNota == Guid.Empty && Objeto.UidSucursal == UidSucursal);
-        //        App.MVProducto.AgregaAlCarrito(UidProducto, new Guid(idSeucursalSeleccionada.Text), new Guid(txtIDSeccion.Text), StrCantidad, 0.0m, Guid.Empty, strNota: StrNotas, URLEmpresa: URLEmpresa, RegistroProductoEnCarrito: productoRepetido.UidRegistroProductoEnCarrito);
-        //    }
-
-
-        //    #region metodo no sirve
-        //    //if (tap)
-        //    //{
-        //    //    tap = false;
-
-        //    //    var Producto = App.MVProducto.ListaDelCarrito.FindAll(Objeto => Objeto.UID == UidProducto && Objeto.UidNota == Guid.Empty && Objeto.UidSucursal == UidSucursal);
-        //    //    if (tipo)
-        //    //    {
-        //    //        Producto = App.MVProducto.ListaDelCarrito.FindAll(Objeto => Objeto.UID == UidProducto && Objeto.UidNota == Guid.Empty && Objeto.UidSucursal == idSeccion);
-        //    //        var productoRepetido = App.MVProducto.ListaDelCarrito.Find(Objeto => Objeto.UID == UidProducto && Objeto.UidNota == Guid.Empty && Objeto.UidSucursal == idSeccion);
-        //    //    }
-        //    //    else
-        //    //    {
-        //    //        Producto = App.MVProducto.ListaDelCarrito.FindAll(Objeto => Objeto.UID == UidProducto && Objeto.UidNota == Guid.Empty && Objeto.UidSucursal == UidSucursal);
-        //    //    }
-        //    //    if (Producto.Count <= 1 && !string.IsNullOrEmpty(StrNotas) || (Producto.Count == 0 && string.IsNullOrEmpty(StrNotas)))
-        //    //    {
-        //    //        //Si solo existe un tarifario en la lista, se muestra al usuario en los datos de la sucursal
-        //    //        if (App.MVTarifario.ListaDeTarifarios.Count > 0)
-        //    //        {
-        //    //            Guid UidTarifario = new Guid();
-        //    //            decimal DmPrecio = 0.0m;
-        //    //            for (int i = 0; i < 1; i++)
-        //    //            {
-        //    //                UidTarifario = App.MVTarifario.ListaDeTarifarios[i].UidTarifario;
-        //    //                DmPrecio = App.MVTarifario.ListaDeTarifarios[i].DPrecio;
-        //    //            }
-        //    //            if (tipo)
-        //    //            {
-        //    //                if (idSeucursalSeleccionada.Text != "")
-        //    //                {
-        //    //                    App.MVProducto.AgregaAlCarrito(UidProducto, new Guid(idSeucursalSeleccionada.Text), UidSucursal, StrCantidad, DmPrecio, UidTarifario, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //    //                }
-        //    //                else
-        //    //                {
-        //    //                    App.MVProducto.AgregaAlCarrito(UidProducto, UidSeccion, UidSucursal, StrCantidad, DmPrecio, UidTarifario, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //    //                }
-        //    //            }
-        //    //            else
-        //    //            {
-        //    //                App.MVProducto.AgregaAlCarrito(UidProducto, UidSucursal, UidSeccion, StrCantidad, DmPrecio, UidTarifario, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //    //            }
-        //    //        } // los datos de la informacion del tarifario se muestran vacios en caso de existir varios registros para esta orden.
-        //    //        else
-        //    //        {
-        //    //            if (tipo)
-        //    //            {
-        //    //                App.MVProducto.AgregaAlCarrito(UidProducto, UidSeccion, UidSucursal, StrCantidad, 0.0m, Guid.Empty, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //    //            }
-        //    //            else
-        //    //            {
-        //    //                App.MVProducto.AgregaAlCarrito(UidProducto, UidSucursal, UidSeccion, StrCantidad, 0.0m, Guid.Empty, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //    //            }
-        //    //        }
-
-        //    //        decimal Subtotal = 0.0m;
-        //    //        decimal Envio = 0.0m;
-        //    //        int cantidad = 0;
-        //    //        foreach (var producto in App.MVProducto.ListaDelCarrito)
-        //    //        {
-        //    //            cantidad = cantidad + producto.Cantidad;
-        //    //            Subtotal = Subtotal + decimal.Parse(producto.StrCosto);
-        //    //        }
-        //    //        foreach (var obj in App.MVProducto.ListaDelInformacionSucursales)
-        //    //        {
-        //    //            Envio = Envio + obj.CostoEnvio;
-        //    //        }
-        //    //        //lblProductosEnCarrito.Text = cantidad.ToString();
-
-        //    //        //Modal de la pagina Empresas.aspx
-        //    //        //if (!string.IsNullOrEmpty(lblUidProductoSeleccionado.Text))
-        //    //        //{
-        //    //        //    ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "Pop", "$('#ModalProducto').modal('hide');", true);
-        //    //        //    DataList DLProductos = PanelCentral.Controls[0].Controls[27].FindControl("DLProductos") as DataList;
-        //    //        //    DLProductos.DataSource = MVProducto.ListaDeProductos;
-        //    //        //    DLProductos.DataBind();
-        //    //        //}
-        //    //        //Modal de la pagina Default.aspx
-        //    //        //if (!string.IsNullOrEmpty(HFUidProducto.Value))
-        //    //        //{
-        //    //        //    ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "Pop", "$('#myModal').modal('hide');", true);
-        //    //        //    DataList DLProductos = PanelCentral.Controls[0].Controls[27].FindControl("DLProductos") as DataList;
-        //    //        //    DLProductos.DataSource = MVProducto.ListaDeProductos;
-        //    //        //    DLProductos.DataBind();
-        //    //        //}
-        //    //        // MuestraDetallesDeLaOrdenGeneral();
-        //    //    }
-        //    //    else
-        //    //    {
-        //    //        //sucursal = Producto[0].UidSucursal;
-        //    //        //seccion = Producto[0].UidSeccion;
-        //    //        //Guid uidProducto, Guid UidSucursal, Guid UidSeccion, string cantidad
-        //    //        //App.MVProducto.AgregaAlCarrito();
-
-        //    //        //var productoRepetido = App.MVProducto.ListaDelCarrito.Find(Objeto => Objeto.UID == UidProducto && Objeto.UidNota == Guid.Empty && Objeto.UidSucursal == UidSucursal);
-        //    //        //App.MVProducto.AgregaAlCarrito(UidProducto, sucursal, seccion, StrCantidad, 0.0m, Guid.Empty, strNota: StrNotas, URLEmpresa: URLEmpresa, RegistroProductoEnCarrito: productoRepetido.UidRegistroProductoEnCarrito);
-
-        //    //        //if (tipo)
-        //    //        //{
-        //    //        //    App.MVProducto.AgregaAlCarrito(UidProducto, UidSeccion, UidSucursal, StrCantidad, 0.0m, Guid.Empty, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //    //        //}
-        //    //        //else
-        //    //        //{
-        //    //        //    App.MVProducto.AgregaAlCarrito(UidProducto, UidSucursal, UidSeccion, StrCantidad, 0.0m, Guid.Empty, strNota: StrNotas, URLEmpresa: URLEmpresa);
-        //    //        //}
-
-        //    //        //App.MVProducto.AgregaAlCarrito(UidProducto, UidSucursal, UidSeccion, StrCantidad, 0.0m, Guid.Empty, strNota: StrNotas);
-        //    //        ////Modal de la pagina Default.aspx
-        //    //        //if (!string.IsNullOrEmpty(lblUidProductoSeleccionado.Text))
-        //    //        //{
-        //    //        //    pnlMensajeProductoSeleccionado.Visible = true;
-        //    //        //    lblMensajeProductoSeleccionado.Text = "Para agregar mas veces este producto de la sucursal seleccionada, gestione los que ya estan dentro del carrito";
-        //    //        //}
-        //    //        ////Modal de la pagina Empresas.aspx
-        //    //        //if (!string.IsNullOrEmpty(HFUidProducto.Value))
-        //    //        //{
-        //    //        //    pnlMensajeProducto.Visible = true;
-        //    //        //    lblMensaje.Text = "Para agregar mas veces este producto  de la sucursal seleccionada, gestione los que ya estan dentro del carrito";
-        //    //        //}
-        //    //    }
-        //    //}
-        //    #endregion
-
-
-
-        //}
         protected void AgregarAlcarrito(Guid UidProducto, Guid UidSucursal, Guid UidSeccion, string StrCantidad, string StrNotas = "")
         {
             #region Guardar el link de imagen
@@ -520,6 +331,7 @@ namespace AppCliente
 
 
         }
+
         private void ButtonMenos_Clicked(object sender, EventArgs e)
         {
             if (cantidad == 1)
