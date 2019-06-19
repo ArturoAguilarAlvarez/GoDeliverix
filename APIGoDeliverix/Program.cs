2a714bc48d0945704b2cc00f14223d12586ae37d
manuel.flores.rodriguez <manuel.flores.rodriguez@hotmail.com>
2019-06-19 16:59:59 -0500

    Implementacion de webApi

--- APIGoDeliverix/Program.cs
+++ APIGoDeliverix/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
+ 
 namespace APIGoDeliverix
 {
     public class Program
