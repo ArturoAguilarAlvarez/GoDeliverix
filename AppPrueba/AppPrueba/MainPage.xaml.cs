31345e0ccab8c34126c2537bb2c676e49a8d13aa
Arturo Aguilar <arturo_365ag@hotmail.com>
2019-06-20 15:30:05 -0500

    Agregar una controlador de ID usuarios

---
+++ AppPrueba/AppPrueba/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppPrueba
+{
+    public partial class MainPage : ContentPage
+    {
+        //http://localhost:63509/api/Profile/Get?Usuario=xxx&Contrasena=xxxasdasd
+        //static HttpClient client = new HttpClient();
+        static HttpClient client = new HttpClient() { BaseAddress = new Uri("http://godeliverix.net") };
+
+        //var httpResponse = await client.GetAsync("api/users/" + "jesulink2514");
+        string product;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            UpdateProductAsync();
+        }
+
+        public async void UpdateProductAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync
+                ("http://localhost:63509/api/Profile/Get?Usuario=xxx&Contrasena=xxxasdasd");
+
+            var httpResponse = await client.GetAsync("api/Profile/" + "jesulink2514");
+
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            string product = await response.Content.ReadAsStringAsync();
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/ProfileController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VistaDelModelo;
+
+namespace WebApplication1.Controllers
+{
+    public class ProfileController : ApiController
+    {
+        #region Propiedades
+        public static VMAcceso MVAcceso = new VMAcceso();
+        #endregion
+
+        // GET: api/Profile
+        public IEnumerable<string> Get(string Usuario, string Contrasena)
+        {
+            if (!string.IsNullOrEmpty(Usuario) && !string.IsNullOrEmpty(Contrasena))
+            {
+                Guid id = Guid.Empty;
+                id = MVAcceso.Ingresar(Usuario, Contrasena);
+                return new string[] { id.ToString() };
+            }
+            else
+            {
+                return new string[] { "NO valido"};
+            }
+            
+        }
+
+        // GET: api/Profile/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Profile
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Profile/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Profile/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
