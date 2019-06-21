506b95da5015dd79b0dd64a71b1d05078cfc0650
manuel.flores.rodriguez <manuel.flores.rodriguez@hotmail.com>
2019-06-20 23:59:24 -0500

    Funciones de la pagina default del cliente agregadas a los controladores de la api.

---
+++ WebApplication1/Controllers/DireccionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.App_Start;
+using VistaDelModelo;
+namespace WebApplication1.Controllers
+{
+    public class DireccionController : ApiController
+    {
+        VMDireccion MVDireccion;
+        ResponseHelper Respuesta;
+        // GET: api/Profile/5
+        public ResponseHelper GetObtenerDireccionCompletaDeSucursal(string UidSucursal)
+        {
+            MVDireccion = new VMDireccion();
+            MVDireccion.ObtenerDireccionSucursal(UidSucursal);
+
+            Respuesta = new ResponseHelper();
+            Respuesta.Data = MVDireccion;
+            Respuesta.Status = true;
+            Respuesta.Message = "Informacion recibida satisfactoriamente";
+            return Respuesta;
+        }
+
+        // POST: api/Profile
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Profile/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Profile/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
