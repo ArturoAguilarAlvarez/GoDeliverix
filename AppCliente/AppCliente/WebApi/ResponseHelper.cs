e6a58bca7f49c3275267a5d0f06d3587b8b97f01
Arturo Aguilar <arturo_365ag@hotmail.com>
2019-06-24 18:40:39 -0500

    Cambios tomar datos api

---
+++ AppCliente/AppCliente/WebApi/ResponseHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCliente.WebApi
+{
+    public  class ResponseHelper
+    {
+        public string Message { set; get; }
+        public bool Status { set; get; }
+        public object Data { set; get; }
+    }
+}
