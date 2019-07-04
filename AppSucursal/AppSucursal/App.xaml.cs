b240c7af2fb57879a903b4106c5e88f8c8b7ca6b
arturoaguilar <arturo_365ag@hotmail.com>
2019-07-04 17:44:44 -0500

    cambios manuel choto

---
+++ AppSucursal/AppSucursal/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppSucursal.Services;
+using AppSucursal.Views;
+
+namespace AppSucursal
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
