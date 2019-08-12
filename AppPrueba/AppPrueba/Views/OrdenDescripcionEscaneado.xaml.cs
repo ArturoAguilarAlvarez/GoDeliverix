ff2d1b844a3b3f490e07afdcdb6c372676b6252b
manuel.flores.rodriguez <manuel.flores.rodriguez@hotmail.com>
2019-08-12 17:55:20 -0500

    12-08-2019

--- AppPrueba/AppPrueba/Views/OrdenDescripcionEscaneado.xaml.cs
+++ AppPrueba/AppPrueba/Views/OrdenDescripcionEscaneado.xaml.cs
@@ -57,7 +57,7 @@ namespace AppPrueba.Views
                 string url = "http://www.godeliverix.net/api/Orden/GetAgregaEstatusALaOrden?UidEstatus=2FDEE8E7-0D54-4616-B4C1-037F5A37409D&StrParametro=S&UidOrden=" + Uidorden + "&UidLicencia=" + AppPrueba.Helpers.Settings.Licencia + "";
                 await _WebApiGoDeliverix.GetAsync(url);
                 url = string.Empty;
-                url = "http://www.godeliverix.net/api/Orden/GetAgregarEstatusOrdenEnSucursal?UidEstatus=2FDEE8E7-0D54-4616-B4C1-037F5A37409D&cTipoDeSucursal=S&UidOrden=" + Uidorden + "&UidLicencia=" + AppPrueba.Helpers.Settings.Licencia + "";
+                url = "http://www.godeliverix.net/api/Orden/GetAgregarEstatusOrdenEnSucursal?UidEstatus=E2BAD7D9-9CD0-4698-959D-0A211800545F&cTipoDeSucursal=S&UidOrden=" + Uidorden + "&UidLicencia=" + AppPrueba.Helpers.Settings.Licencia + "";
                 await _WebApiGoDeliverix.GetAsync(url);
 
                 //App.MVOrden.AgregarEstatusOrdenEnSucursal(new Guid("0E08DE81-DED2-41BE-93A5-A3742C3C411F"), "S", AppPrueba.Helpers.Settings.Licencia, UidOrden: Uidorden);
@@ -66,12 +66,11 @@ namespace AppPrueba.Views
 
                 //App.MVOrden.BuscarOrdenes("Sucursal", UidLicencia: new Guid(AppPrueba.Helpers.Settings.Licencia), EstatusSucursal: "Lista a enviar", TipoDeSucursal: "S");
                 url = string.Empty;
-                url = ("http://www.godeliverix.net/api/Orden/GetOrdenesSucursal?Licencia=" + AppPrueba.Helpers.Settings.Licencia.ToString() + "&Estatus=Lista%20a%20enviar&tipoSucursal=s");
+                url = ("http://www.godeliverix.net/api/Orden/GetOrdenesSucursal?Licencia=" + AppPrueba.Helpers.Settings.Licencia.ToString() + "&Estatus=Listaaenviar&tipoSucursal=s");
                 string DatosObtenidos = await _WebApiGoDeliverix.GetStringAsync(url);
                 var DatosGiros = JsonConvert.DeserializeObject<ResponseHelper>(DatosObtenidos).Data.ToString();
                 App.MVOrden = JsonConvert.DeserializeObject<VistaDelModelo.VMOrden>(DatosGiros);
 
-                MyListviewOrdenesPorEnviar.ItemsSource = App.MVOrden.ListaDeOrdenes;
                 await Application.Current.MainPage.Navigation.PopToRootAsync();
             }
         }
