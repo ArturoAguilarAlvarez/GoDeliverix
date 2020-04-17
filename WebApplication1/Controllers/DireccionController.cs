347f8590c965b554de2c0f45d2817cf44b791e72
Manuel <manuelfloresrodriguez1@gmail.com>
2020-04-17 16:45:34 -0500

    17/04/2020
    Api funcionando para la version demo del cliente movil

--- WebApplication1/Controllers/DireccionController.cs
+++ WebApplication1/Controllers/DireccionController.cs
@@ -144,7 +144,7 @@ namespace WebApplication1.Controllers
             return Respuesta;
         }
 
-        public ResponseHelper GetObtenerDireccionConDatosDeGoogle(string StrNombreCiudad,string CodigoEstado,string CodigoPais, string Latitud, string Longitud)
+        public ResponseHelper GetObtenerDireccionConDatosDeGoogle(string StrNombreCiudad,string CodigoEstado,string CodigoPais)
         {
             MVDireccion = new VMDireccion();
             Respuesta = new ResponseHelper();
