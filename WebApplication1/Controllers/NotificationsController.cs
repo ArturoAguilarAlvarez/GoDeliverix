486052a80f64077755537674899d132fb99dafa0
Hector <hectorpech@compuandsoft.com>
2020-09-22 11:38:58 -0500

    controlador para notificaciones

---
+++ WebApplication1/Controllers/NotificationsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VistaDelModelo;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace WebApplication1.Controllers
+{
+    public class NotificationsController : ApiController
+    {
+        private NotificationViewModel viewModel = new NotificationViewModel();
+
+        public NotificationsController()
+        {
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult ReadAllCustomer(string uid)
+        {
+            try
+            {
+                var result = this.viewModel.ReadAllNotifications(Modelo.Enums.NotificationTarget.Cliente, Guid.Parse(uid));
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
