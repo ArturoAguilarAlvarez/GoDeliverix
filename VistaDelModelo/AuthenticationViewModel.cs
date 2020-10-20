3435fa97fe300a8a24cc807720e17891f7105ec1
Hector <hectorpech@compuandsoft.com>
2020-10-19 21:05:55 -0500

    Controlador y vista del modelo para la autenticacion del repartidor

---
+++ VistaDelModelo/AuthenticationViewModel.cs
@@ -0,0 +1,53 @@
+﻿using DBControl;
+using Modelo.ApiResponse;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VistaDelModelo
+{
+    public class AuthenticationViewModel
+    {
+        private AuthenticationDataAccess DbAuthentication { get; }
+
+        public AuthenticationViewModel()
+        {
+            this.DbAuthentication = new AuthenticationDataAccess();
+        }
+
+        /// <summary>
+        /// Iniciar session del repartidor
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="profileUid"></param>
+        /// <returns></returns>
+        public DeliveryManLoginResult AuthenticateDeliveryMan(string username, string password, Guid profileUid)
+        {
+            DeliveryManLoginResult result = null;
+            DataTable data = this.DbAuthentication.AuthenticateDeliveryMan(username, password, profileUid);
+
+            if (data.Rows.Count > 0 || data.Rows.Count == 1)
+            {
+                foreach (DataRow row in data.Rows)
+                {
+                    result = new DeliveryManLoginResult()
+                    {
+                        Uid = (Guid)row["UidUsuario"],
+                        UidPerfil = (Guid)row["UidPerfil"],
+                        Usuario = (string)row["Usuario"],
+                        CorreoElectronico = (string)row["Correo"],
+                        FechaNacimiento = (DateTime)row["FechaDeNacimiento"],
+                        Nombre = row.IsNull("Nombre") ? "" : (string)row["Nombre"],
+                        ApellidoPaterno = row.IsNull("ApellidoPaterno") ? "" : (string)row["ApellidoPaterno"],
+                        ApellidoMaterno = row.IsNull("ApellidoMaterno") ? "" : (string)row["ApellidoMaterno"]
+                    };
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using VistaDelModelo;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthController : ApiController
+    {
+        private AuthenticationViewModel AuthenticationVm { get; }
+
+        public AuthController()
+        {
+            this.AuthenticationVm = new AuthenticationViewModel();
+        }
+
+        /// <summary>
+        /// Metodo para autenticación de los repartidores
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult Login_DeliveryMan([FromBody] DeliveryManLogin login)
+        {
+            try
+            {
+                var result = this.AuthenticationVm.AuthenticateDeliveryMan(login.Username, login.Password, login.ProfileUid);
+
+                if (result == null)
+                {
+                    return BadRequest("User does not exist");
+                }
+
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+
+    public class DeliveryManLogin
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public Guid ProfileUid { get; set; }
+    }
+}
