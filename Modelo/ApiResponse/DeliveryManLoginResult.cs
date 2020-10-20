4237a880ba5cc73ce5304f9bbc46c96efc6f0618
Hector <hectorpech@compuandsoft.com>
2020-10-19 21:04:48 -0500

    modelos de respuesta del api

---
+++ Modelo/ApiResponse/DeliveryManLoginResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.ApiResponse
+{
+    public class DeliveryManLoginResult
+    {
+        public Guid Uid { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string ApellidoPaterno { get; set; }
+
+        public string ApellidoMaterno { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public Guid UidPerfil { get; set; }
+
+        public string CorreoElectronico { get; set; }
+    }
+}
