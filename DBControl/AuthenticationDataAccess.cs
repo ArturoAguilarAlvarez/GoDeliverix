5dc8a24392f928f2f88907875926b837d038cd6b
Hector <hectorpech@compuandsoft.com>
2020-10-19 21:04:36 -0500

    acceso a la base de datos (repartidor)

---
+++ DBControl/AuthenticationDataAccess.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBControl
+{
+    /// <summary>
+    /// Realizar consultas y manipulacion de datos 
+    /// </summary>
+    public class AuthenticationDataAccess
+    {
+        /// <summary>
+        /// Conexion a la base de datos
+        /// </summary>
+        protected readonly Conexion dbConexion;
+
+        public AuthenticationDataAccess()
+        {
+            this.dbConexion = new Conexion();
+        }
+
+        /// <summary>
+        /// Validar acceso del repartidor
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="profileUid"></param>
+        /// <returns></returns>
+        public DataTable AuthenticateDeliveryMan(string username, string password, Guid profileUid)
+        {
+            string query = $@"
+                SELECT 
+                    U.[UidUsuario],
+                    U.[Usuario],
+                    U.[Nombre],
+                    U.[ApellidoPaterno],
+                    U.[ApellidoMaterno],
+                    U.[FechaDeNacimiento],
+                    U.[UidPerfil],
+                    C.[Correo]
+                FROM [Usuarios] AS U
+                    INNER JOIN [CorreoUsuario] AS CU ON CU.[UidUsuario] = U.[UidUsuario]
+                    INNER JOIN [CorreoElectronico] AS C ON C.[IdCorreo] = CU.[UidCorreo]
+                WHERE [Usuario] = '{username}' 
+                    AND [Contrasena] COLLATE Latin1_General_CS_AS =  '{password}'
+                    AND [UidPerfil] = '{profileUid.ToString()}'";
+
+            DataTable data = this.dbConexion.Consultas(query);
+            return data;
+        }
+    }
+}
