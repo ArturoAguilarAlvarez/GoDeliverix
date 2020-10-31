9add00256806989445f325664fb89feb375c49f4
Hector <hectorpech@compuandsoft.com>
2020-10-31 10:03:47 -0500

    Application Constants

---
+++ Modelo/Common/ApplicationConstants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Common
+{
+    public static class ApplicationConstants
+    {
+        /// <summary>
+        /// Firebase Data Base Server Url
+        /// </summary>
+        public static string FirebaseBaseUrl = "https://godeliverix-customers.firebaseio.com/";
+
+        /// <summary>
+        /// Firebase Secret Key
+        /// <see cref="https://stackoverflow.com/questions/37418372/firebase-where-is-my-account-secret-in-the-new-console"/>
+        /// </summary>
+        public static string FirebaseSecretKey = "";
+    }
+}
