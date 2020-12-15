6f2020848741ad836bb3a3f812b26422c61d3cc3
Hector <hectorpech@compuandsoft.com>
2020-12-14 19:30:10 -0500

    Uid estaticos

---
+++ Modelo/Common/ConceptoUid.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modelo.Common
+{
+    public static class ConceptoUid
+    {
+        public static Guid TarjetaPrepago = Guid.Parse("5CCE4BD9-3AC4-40A5-A0A9-FA0234A3F848");
+        public static Guid DepositoBancario = Guid.Parse("C7233E52-74CC-434E-9D37-AA170848FC7A");
+        public static Guid PagoEnGoDeliverix = Guid.Parse("DCA75F23-5DDC-4EA5-B088-6D5B187F76F4");
+        public static Guid Rembolso = Guid.Parse("2AABDF7F-EDCE-455F-B775-6283654D7DA0");
+    }
+}
---
+++ Modelo/Common/TipoMovimientoUid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Modelo.Common
+{
+    public static class TipoMovimientoUid
+    {
+        public static Guid Agregar = Guid.Parse("E85F0486-1FBE-494C-86A2-BFDDC733CA5D");
+        public static Guid Retirar = Guid.Parse("6C7F4C2E-0D27-4200-9485-7BE331066D33");
+    }
+}
