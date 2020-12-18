d4848575f90bbb25aa2ca2162e2ef3db6aeb78bc
Hector <hectorpech@compuandsoft.com>
2020-12-17 19:25:26 -0500

    vista del modelo

---
+++ VistaDelModelo/PaymentViewModel.cs
@@ -0,0 +1,34 @@
+﻿using DBControl;
+using Modelo.ApiResponse;
+using Modelo.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VistaDelModelo
+{
+    public class PaymentViewModel
+    {
+        private PaymentDataAccess PaymentDb { get; }
+
+        public PaymentViewModel()
+        {
+            this.PaymentDb = new PaymentDataAccess();
+        }
+
+
+        public bool Add(Payment payment)
+        {
+            bool result = this.PaymentDb.Registry(Guid.NewGuid(), payment.UidFormaCobro, payment.UidOrden, payment.UidEstatusCobro, payment.Monto, payment.DescuentoMonedero);
+
+            if (result && payment.DescuentoMonedero.HasValue)
+            {
+                this.PaymentDb.RegistryWalletTransaction(payment.UidUsuario, TipoMovimientoUid.Retirar, ConceptoUid.PagoEnGoDeliverix, payment.UidDireccion, payment.DescuentoMonedero.Value);
+            }
+
+            return result;
+        }
+    }
+}
