ca981210455daca1eae7eb933809905178712bde
Hector <hectorpech@compuandsoft.com>
2020-12-17 19:25:10 -0500

    modelo

---
+++ Modelo/ApiResponse/Payment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.ApiResponse
+{
+    public class Payment
+    {
+        public Guid UidPago { get; set; }
+
+        public Guid UidUsuario { get; set; }
+
+        public Guid UidFormaCobro { get; set; }
+
+        public Guid UidDireccion { get; set; }
+
+        public Guid UidEstatusCobro { get; set; }
+
+        public Guid UidOrden { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public decimal? DescuentoMonedero { get; set; }
+    }
+}
