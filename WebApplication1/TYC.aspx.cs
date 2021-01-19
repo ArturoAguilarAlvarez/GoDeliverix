7364bc2b1d3609cd3439bac31edd384ffba276c8
Manuel <manuelfloresrodriguez1@gmail.com>
2021-01-19 16:26:26 -0500

    Ventana de terminos y condiciones

---
+++ WebApplication1/TYC.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Net.Http;
+namespace WebApplication1
+{
+    public partial class TYC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string line;
+            string url = "";
+            url = Request["url"];
+            StreamReader s = URLStream(url);
+            String myline = s.ReadToEnd(); //First Line
+            pnlPrincipal.InnerHtml = myline;
+        }
+        static StreamReader URLStream(String fileurl)
+        {
+            return new StreamReader(new HttpClient().GetStreamAsync(fileurl).Result);
+        }
+    }
+}
