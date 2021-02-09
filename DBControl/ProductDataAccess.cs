c54778f385a4550eafc0905e13f8cd398d6f54bf
Manuel <manuel.flores.rodriguez@hotmail.com>
2021-02-08 22:18:36 -0500

    .26: Auto stash before merge of "ViOSCliente1.26" and "origin/ViOSCliente1.26"

--- DBControl/ProductDataAccess.cs
+++ DBControl/ProductDataAccess.cs
@@ -482,9 +482,112 @@ ORDER BY {order}
 OFFSET @pageSize * @pageNumber ROWS
 FETCH NEXT @pageSize ROWS ONLY";
 
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+
+            command.Parameters.AddWithValue("@pageSize", pageSize);
+            command.Parameters.AddWithValue("@pageNumber", pageNumber);
+            command.Parameters.AddWithValue("@UidColonia", uidColonia);
+            command.Parameters.AddWithValue("@UidEstado", uidEstado);
+            command.Parameters.AddWithValue("@Dia", dia);
+            command.Parameters.AddWithValue("@FilterType", tipoFiltro);
+            command.Parameters.AddWithValue("@UidFilter", uidFiltro);
+
+            if (!string.IsNullOrEmpty(filtro))
+            {
+                command.Parameters.AddWithValue("@Filter", filtro);
+                where += " AND e.NombreComercial like '%'+@Filter+'%' ";
+            }
+
+            if (tipoFiltro.Equals("Giro") && !string.IsNullOrEmpty(tipoFiltro))
+            {
+                filterJoin = " INNER JOIN GiroProducto gp on gp.UidProducto = p.UidProducto ";
+                filterWhere = " AND gp.UidGiro = '" + uidFiltro + "' ";
+            }
+            else if (tipoFiltro.Equals("Categoria") && !string.IsNullOrEmpty(tipoFiltro))
+            {
+                filterJoin = " INNER JOIN CategoriaProducto cp on cp.UidProducto = p.UidProducto ";
+                filterWhere = " AND cp.UidCategoria = '" + uidFiltro + "' ";
+            }
+            else if (tipoFiltro.Equals("Subcategoria") && !string.IsNullOrEmpty(tipoFiltro))
+            {
+                filterJoin = " INNER JOIN SubcategoriaProducto scp on scp.UidProducto = p.UidProducto ";
+                filterWhere = " AND scp.UidSubcategoria = '" + uidFiltro + "' ";
+            }
+            else if (tipoFiltro.Equals("None") || string.IsNullOrEmpty(tipoFiltro))
+            {
+                filterJoin = " INNER JOIN GiroProducto gp on gp.UidProducto = p.UidProducto ";
+            }
+
+            string order = (string.IsNullOrEmpty(sortField) || string.IsNullOrEmpty(sortOrder)) ? " Uid " : $"{sortField} {sortOrder.ToUpper()}";
+
+            string query = $@"
+            -- Zona horaria del usuario acorde al estado
+            DECLARE @TimeZone VARCHAR(50);
+            -- Fecha y Hora local del usuario
+            DECLARE @UserDateTime DATETIME;
+            -- Hora actual del usuario
+            DECLARE @UserTime VARCHAR(20);
+
+            -- Obtener zona horaria del estado
+            SELECT
+                @TimeZone = Z.IdZonaHoraria
+            FROM [ZonaHoraria] AS Z
+                INNER JOIN [ZonaHorariaPais] AS P ON P.[IdZonaHoraria] = Z.[IdZonaHoraria]
+                INNER JOIN [ZonaHorariaEstado] AS E ON E.[UidRelacionZonaPaisEstado] = P.[UidZonaHorariaPais]
+            WHERE E.UidEstado = @UidEstado
+
+            -- Obtener DateTime del la zona horaria
+            SELECT @UserDateTime = SYSDATETIMEOFFSET() AT TIME ZONE @TimeZone 
+
+            -- Obtener Time del DateTime
+            SELECT @UserTime = CONVERT(VARCHAR, @UserDateTime, 8)
+
+            SELECT *, [Count] = COUNT (*) OVER() FROM (
+                SELECT 
+                    DISTINCT 
+                    p.uidproducto AS [Uid], 
+                    i.NVchRuta AS [ImgUrl],
+                    (select top 1 i.NVchRuta from imagenes i inner join imagenempresa ie on ie.uidImagen = i.uidimagen where ie.uidempresa = e.uidempresa and i.NVchRuta like'%FotoPerfil%') as [CompanyImgUrl],  
+                    p.VchNombre AS [Name], 
+                    p.VchDescripcion AS [Description],
+                    e.UidEmpresa AS [UidCompany],
+                    e.NombreComercial AS [CompanyName],
+                    dbo.ObtenerMenorPrecioPorProducto(@FilterType,@Dia,@UidFilter,p.uidproducto,@UidColonia,@UidEstado) AS [Price]
+                FROM [Productos] p 
+	                INNER JOIN ImagenProducto ip on ip.UidProducto = p.UidProducto 
+	                INNER JOIN Imagenes i on ip.UidImagen = i.UIdImagen 
+	                INNER JOIN SeccionProducto sp on sp.UidProducto = p.UidProducto 
+	                INNER JOIN Seccion se on se.UidSeccion = sp.UidSeccion 
+	                INNER JOIN Oferta O on O.UidOferta = se.UidOferta 
+	                INNER JOIN DiaOferta DO on DO.UidOferta = O.UidOferta 
+	                INNER JOIN Dias D on D.UidDia = DO.UidDia 
+	                INNER JOIN Sucursales s on s.UidSucursal = o.Uidsucursal 
+	                INNER JOIN ContratoDeServicio CDS on CDS.UidSucursalSuministradora = s.UidSucursal 
+	                INNER JOIN turnosuministradora ts on ts.uidsucursal = CDS.UidSucursalSuministradora and ts.dtmhorafin is null
+	                INNER JOIN TurnoDistribuidora td on td.UidSucursal = CDS.UidSucursalDistribuidora and td.DtmHoraFin is null
+	                INNER JOIN ZonaDeRepartoDeContrato ZDRC on ZDRC.UidContrato = CDS.UidContrato 
+	                INNER JOIN Tarifario t on t.UidRegistroTarifario = ZDRC.UidTarifario 
+	                INNER JOIN ZonaDeServicio zd on zd.UidColonia = @UidColonia and zd.UidRelacionZonaServicio = t.UidRelacionZonaEntrega 
+	                INNER JOIN Empresa e on e.UidEmpresa = p.UidEmpresa 
+                    {filterJoin}
+                WHERE 
+                    @UserTime between se.VchHoraInicio and se.VchHoraFin
+	                and zd.UidColonia = @UidColonia 
+	                and p.IntEstatus = 1 and D.VchNombre = @Dia and O.IntEstatus = 1 and se.IntEstatus = 1 
+	                and sp.VchTiempoElaboracion is not null and CDS.UidEstatusContrato = 'CD20F9BF-EBA2-4128-88FB-647544457B2D'
+	                and s.UidSucursal in ( select s.UidSucursal from Sucursales s inner join Direccion d on d.UidDireccion = s.UidDireccion inner join ZonaHorariaEstado ZHE on ZHE.UidEstado = d.UidEstado	inner join ZonaHorariaPais ZHP on ZHP.UidZonaHorariaPais = ZHE.UidRelacionZonaPaisEstado inner join Empresa e on e.UidEmpresa = s.UidEmpresa where  @UserTime between s.HorarioApertura and s.HorarioCierre  and ZHP.IdZonaHoraria = @TimeZone) 
+                    and e.IdEstatus = 1 
+                    and s.IntEstatus = 1
+                    {filterWhere} {where}
+            ) payload 
+            ORDER BY {order}
+            OFFSET @pageSize * @pageNumber ROWS
+            FETCH NEXT @pageSize ROWS ONLY";
+
             command.CommandText = query;
 
-            DataTable data = this.dbConexion.Consultas(query);
+            DataTable data = this.dbConexion.Busquedas(command);
             return data;
         }
     }
