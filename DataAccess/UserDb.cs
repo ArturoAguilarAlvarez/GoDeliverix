9a8c944ef485209eb11c9179b2610aad54c9c407
Hector <hectorpech@compuandsoft.com>
2021-03-17 18:48:59 -0500

    Datos del usuario

---
+++ DataAccess/UserDb.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using DataAccess.Common;
+using Modelo.ApiResponse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class UserDb : BaseDapper
+    {
+        public UserDb() { }
+
+        public User GetUser(Guid uid)
+        {
+            string query = @"
+SELECT 
+    U.UidUsuario AS [Uid],
+    U.UidPerfil AS ProfileUid,
+    U.Nombre AS [Name],    
+    U.ApellidoPaterno AS FirstLastName,
+    U.ApellidoMaterno AS SecondLastName,
+    CE.Correo As Email,
+    U.FechaDeNacimiento AS Birthday
+FROM Usuarios AS U
+    INNER JOIN CorreoUsuario AS CU ON CU.UidUsuario = U.UidUsuario
+    INNER JOIN CorreoElectronico AS CE ON CE.IdCorreo = CU.UidCorreo
+WHERE U.UidUsuario = @UidUsuario";
+
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@UidUsuario", uid);
+
+            return this.QuerySingleOrDefault<User>(query,parameters);
+        }
+    }
+}
--- WebApplication1/Controllers/v2/v2ProfilesController.cs
+++ WebApplication1/Controllers/v2/v2ProfilesController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using VistaDelModelo;
 
 namespace WebApplication1.Controllers.v2
 {
     [Route("api/v2/Profiles/{action}")]
     public class v2ProfilesController : ApiController
     {
+        private UserViewModel UserVm { get; }
+
         public v2ProfilesController()
         {
+            this.UserVm = new UserViewModel();
+        }
 
+        [HttpGet]
+        public IHttpActionResult Get(Guid uid)
+        {
+            try
+            {
+                var result = this.UserVm.GetAllByUserId(uid);
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
