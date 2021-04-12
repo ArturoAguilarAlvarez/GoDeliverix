15ea478a2c6881afa6b18c291b03e71347b926a2
Hector <hectorpech@compuandsoft.com>
2021-04-12 18:11:48 -0500

    Entity type enum

---
+++ Modelo/Enums/EntityType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Enums
+{
+    /// <summary>
+    /// Enumerador con las entidades registradas en el sistema, se usa para generar referencias en tablas Pivote e identifier en que tabla realizar la busqueda
+    /// !NUNCA CAMBIAR EL ORDEN DE LOS ITEMS!
+    /// </summary>
+    public enum EntityType
+    {
+        None = -1,
+        Empresa,
+        Sucursal,
+        Productos,
+        Monedero,
+        Movimientos,
+        Orden,
+        OndenSucursal,
+        UserSignInRewardCode,
+        AllUserSignInRewardCode
+    }
+}
