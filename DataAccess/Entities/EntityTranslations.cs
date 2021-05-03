f822eeaa30aee78798bdc847a8495b28ce9a0355
Hector <hectorpech@compuandsoft.com>
2021-05-03 17:52:15 -0500

    Modelo de traudcciones

---
+++ DataAccess/Entities/EntityTranslations.cs
@@ -0,0 +1,19 @@
+﻿using Modelo.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class EntityTranslations
+    {
+        public int Uid { get; set; }
+        public EntityType EntityType { get; set; }
+        public Guid EntityUid { get; set; }
+        public string Key { get; set; }
+        public string En { get; set; }
+        public string Es { get; set; }
+    }
+}
